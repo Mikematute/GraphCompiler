634039b74d92996146a8480be840e78b1cd01a5d
guillermoAMS <ams.guillermo@gmail.com>
2018-03-03 17:34:04 -0600

    All rules added

--- yacc_c.py
+++ yacc_c.py
@@ -130,10 +130,11 @@ import ply.yacc as yacc
 
 # from calclex import tokens
 
+################################ P R O G R A M ################################
 def p_program(t):
     'program : ID SCOLO vars function body'
     # print("enters program")
-
+################################### V A R S ####################################
 def p_vars(t):
     '''vars : VAR type vars_1 SCOLO vars
             | empty'''
@@ -149,7 +150,7 @@ def p_vars_1(t):
         # print("enter ID COMA ID")
     # else:
         # print("enter ID")
-
+############################### F U N C T I O N ################################
 def p_function(t):
     '''function : function_t ID LPAREN function_v RPAREN LBRACK vars statutes RBRACK function
                 | empty'''
@@ -169,10 +170,10 @@ def p_function_v(t):
 def p_function_v1(t):
     '''function_v1 : type ID COMA
                    | type ID COMA function_v1'''
-
+#################################### B O D Y ###################################
 def p_body(t):
     'body : MAIN LPAREN RPAREN LBRACK vars statutes RBRACK'
-
+#################################### T Y P E ###################################
 def p_type(t):
     '''type : t_number
             | t_string
@@ -209,7 +210,7 @@ def p_t_array_1(t):
 def p_t_array_2(t):
     '''t_array_2 : LCORCH CTE_INT RCORCH t_array_2
                  | empty'''
-
+################################ S T A T U T E S ###############################
 def p_t_statutes(t):
     '''statutes : statutes_1 statutes
                 | empty'''
@@ -219,12 +220,118 @@ def p_t_statutes_1(t):
                   | writing
                   | condition
                   | cycle'''
-
+#---------------------------- a s s i g n a t i o n ----------------------------
 def p_assignation(t):
     'assignation : ID EQL expression'
+#------------------------------- w r i t i n g ---------------------------------
+def p_writing(t):
+    'writing : PRINT LPAREN writing_1 RPAREN SCOLO'
 
+def p_writing_1(t):
+    '''writing_1 : expression
+                 | CTE_STRING
+                 | writing_2'''
 
+def p_writing_2(t):
+    '''writing_2 : expression SUMA writing_1
+                 | CTE_STRING SUMA writing_1'''
+#------------------------------ c o n d i t i o n ------------------------------
+def p_condition(t):
+    'condition : IF LPAREN expression RPAREN LBRACK statutes RBRACK condition_1 SCOLO'
 
+def p_condition_1(t):
+    '''condition_1 : ELSE LBRACK expression RBRACK
+                   | empty'''
+#--------------------------------- c y c l e -----------------------------------
+def p_cycle(t):
+    '''cycle : c_while
+             | c_do
+             | c_for
+             | c_forin'''
+
+def p_while(t):
+    'c_while : WHILE LPAREN expression RPAREN LBRACK statutes RBRACK'
+
+def p_do(t):
+    'c_do : DO LBRACK statutes RBRACK WHILE LPAREN expression RPAREN'
+
+def p_for(t):
+    'c_for : FOR LPAREN ID SCOLO expression SCOLO assignation RPAREN LBRACK statutes RBRACK'
+
+def p_forin(t):
+    'c_forin : FOR LPAREN ID IN ID RPAREN LBRACK statutes RBRACK'
+
+############################## E X P R E S S I O N #############################
+def p_expression(t):
+    '''expression : exp_lv1
+                  | exp_lv1 AND expression
+                  | exp_lv1 OR expression'''
+
+def p_exp_lv1(t):
+    'exp_lv1 : exp_lv2 exp_lv1_1'
+def p_exp_lv1_1(t):
+    '''exp_lv1_1 : LESST exp_lv2
+                 | MORET exp_lv2
+                 | LESSEQUAL exp_lv2
+                 | MOREEQUAL exp_lv2
+                 | EQUALTO exp_lv2
+                 | NOTEQUALTO exp_lv2
+                 | empty'''
+
+def p_exp_lv2(t):
+    '''exp_lv2 : exp_lv3
+               | exp_lv3 SUMA exp_lv2
+               | exp_lv3 SUB exp_lv2'''
+
+def p_exp_lv3(t):
+    '''exp_lv3 : exp_lv4
+               | exp_lv4 MUL exp_lv3
+               | exp_lv4 DIV exp_lv3
+               | exp_lv4 RESD exp_lv3'''
+
+def p_exp_lv4(t):
+    '''exp_lv4 : exp_lv5
+               | NOT exp_lv5'''
+
+def p_exp_lv5(t):
+    '''exp_lv5 : RPAREN expression LPAREN
+               | var_cte
+               | method
+               | ID'''
+
+################################# V A R _ C T E ################################
+def p_var_cte(t):
+    '''var_cte : CTE_INT
+               | CTE_FLO
+               | CTE_BOO
+               | CTE_STRING
+               | CTE_CHAR'''
+
+################################## M E T H O D #################################
+def p_method(t):
+    'method : ID DOT method_t LPAREN method_1 RPAREN'
+
+def p_method_1(t):
+    '''method_1 : method_1_1
+                | empty'''
+
+def p_method_1_1(t):
+    '''method_1_1 : method_v
+                  | method_v COMA method_1_1'''
+
+def p_method_t(t):
+    '''method_t : DEG
+                | SHORTPATH
+                | DIAMETER
+                | ADD
+                | DELETE
+                | ARC'''
+
+def p_method_v(t):
+    '''method_v : ID
+                | LBRACK ID COMA ID RBRACK'''
+
+################################## O T H E R S #################################
 def p_empty(p):
     'empty :'
     pass
