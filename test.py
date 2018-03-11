8f990c1a43d9601735f813a03382426a2325fa77
Mikematute <migueblack@gmail.com>
2018-03-11 12:48:11 -0600

    oracle

---
+++ test.py
@@ -0,0 +1,9 @@
+################################################################################
+# Test File used for different tests.
+# Random
+# Not to be evaluated
+################################################################################
+
+from oracle import consult
+
+print(consult(0,7,1))
